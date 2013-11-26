050562708b88476409f6673ee48a2b2029c53589
Andy <andylu882002@yahoo.comt>
2013-11-26 00:58:28 -0500

    This is initial file for sale

---
+++ Srcmain/MySales/Main.java
@@ -0,0 +1,18 @@
+package MySales;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData mydata=new SalesData();
+		displaysale();
+		mydata.getdata();
+
+	}
+	
+	public static void displaysale()
+	{
+		System.out.println("My good sale is great!!!");
+		System.out.println("I complete all sale today!!");
+	}
+
+}
---
+++ Srcmain/MySales/SalesData.java
@@ -0,0 +1,8 @@
+package MySales;
+public class SalesData {
+	public void getdata()
+	{
+		System.out.println("This data $89000 sale for today");
+	}
+
+}
